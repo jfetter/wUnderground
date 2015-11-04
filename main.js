1c0585d07b9cbf01dcb155b29f0bd24fd297a126
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-04 08:19:28 -0800

    background looks nicer at initialize

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ $(document).ready(init);
 function init(){
 	
 	console.log("I'm init");
-	$("#getZip").click(getInfoForZip);
+
 	$("#zip").data("value", defaultZip());
+	$("#getZip").click(getInfoForZip);
 	$(".header").mouseover(function(){
 		$(".heading").toggleClass("invisible");
 		})
@@ -20,6 +21,7 @@ function defaultZip(){
 		 	$("#zip").val(data.location.zip);
 	});
 }
+
 function getInfoForZip(){
  	var userZip = $("#zip").val();
    //if the user input is not valid ask them to do it again
@@ -34,25 +36,28 @@ function getInfoForZip(){
 		 		var temp = parseFloat(data.current_observation.temp_f);
 		 		var	icon = (data.current_observation.icon_url);
 		 		var	forecast = (data.current_observation.forecast_url);
-				console.log( data);
-				 if (parseInt(temp) < 80){	
-				 	$("#hotOrCold").text("Nah, but you are one cool cucumber!");
+		 		console.log( data);
+		 					// })
+ 				 		// $.get(logo.url)
+				 if (parseInt(temp) > 80){
+				 	$("#hotOrCold").text("Whoa!" + temp + "degrees! " + 
+				 		"You definitely aren't hot, but you are one cool cucumber!");
 				 	$(".coolCue").removeClass("invisible");
-				 	$("#wrapper").removeClass("summer").addClass("winter").css("color", "white");
+				 	$("#wrapper").removeClass("summer").addClass("winter");
  					$("#vacationIdeas").attr("href", "https://www.airbnb.com/s/Lake-Tahoe--United-States?af=8781787&c=p2_d_eng_laketahoe_p2&gclid=CjwKEAiA9uaxBRDYr4_hrtC3tW8SJAD6UU8G0NtnffCX2bpOjigNIPJ6r9Z0tEwGdplAVkVmOT0vjBoCQUHw_wcB&dclid=CNrwqN-19sgCFaQSRQod3P4M7A")
- 						.text("chill out");				
+ 						.text("hot time in a new town... huh... huhhhh?");
  									}else{
-					var hot = false;
-					$("#hotOrCold").text("Yeah you are! You are one hawt tamale!");
+					$("#hotOrCold").text("Yeah you arrrre!  " + temp + "degrees!             " +
+						"     One hawt tamale that's what you are");
 					$(".hottie").removeClass("invisible");
 					$("#wrapper").removeClass("winter").addClass("summer");
-				}
 					$("#vacationIdeas").attr("href", "https://www.airbnb.com/?af=43720035&c=A_TC%3Djmydw8thr5%26G_MT%3Db%26G_CR%3D19944542536%26G_N%3Dg%26G_K%3D%2Bair%20%2Bbnb%26G_P%3D%26G_D%3Dc&gclid=CjwKEAiA9uaxBRDYr4_hrtC3tW8SJAD6UU8GiANrBwuak5j42k1L9KrT7_Q-8PzbUixzhajnnRZHDRoCrm3w_wcB&dclid=CKzjtYS39sgCFTEHRQodbVcByA")
-						.text("hot time in a new town... huh... huhhhh?")
-					console.log($("#temp").text(temp));
-				 	$("#icon").attr("src", icon);
-					$("#forecast").attr("href", forecast).text("Forecast");
-			})
+						 .text("chill out");				
+					}
+					$("#temp").text("current temp " + temp + "F");	
+					$("#icon").attr("src", icon), 
+					$("#forecast").attr("href", forecast).text("Click To View Full Forecast")
+	})
 			.fail(function(promise, status, error){
 				console.log("promise:", promise);
 				console.log("error:", error);
