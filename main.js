aec31aa0d6d8797df6dfc227ab9327c2eab50189
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-04 09:00:23 -0800

    a little more styled

--- main.js
+++ main.js
@@ -39,20 +39,22 @@ function getInfoForZip(){
 		 		console.log( data);
 		 					// })
  				 		// $.get(logo.url)
-				 if (parseInt(temp) > 80){
-				 	$("#hotOrCold").text("Whoa!" + temp + "degrees! " + 
-				 		"You definitely aren't hot, but you are one cool cucumber!");
+				 if (parseInt(temp) < 80){
+				 	$("#hotOrCold").text("Whoa!" + temp + " degrees! " + 
+				 		"You definitely aren't hot, but you are one cool cucumber!").removeClass("hottie");
 				 	$(".coolCue").removeClass("invisible");
+				 	$("#coolCue").attr("src", "https://dirtyfilthyhealthy.files.wordpress.com/2013/05/screen-shot-2013-05-19-at-17-33-50.png?w=560");
 				 	$("#wrapper").removeClass("summer").addClass("winter");
  					$("#vacationIdeas").attr("href", "https://www.airbnb.com/s/Lake-Tahoe--United-States?af=8781787&c=p2_d_eng_laketahoe_p2&gclid=CjwKEAiA9uaxBRDYr4_hrtC3tW8SJAD6UU8G0NtnffCX2bpOjigNIPJ6r9Z0tEwGdplAVkVmOT0vjBoCQUHw_wcB&dclid=CNrwqN-19sgCFaQSRQod3P4M7A")
- 						.text("hot time in a new town... huh... huhhhh?");
+ 						.text("hot time in a new town... huh... huhhhh?").attr("type", "button");
  									}else{
-					$("#hotOrCold").text("Yeah you arrrre!  " + temp + "degrees!             " +
+					$("#hotOrCold").text("Yeah you arrrre!  " + temp + " degrees!             " +
 						"     One hawt tamale that's what you are");
 					$(".hottie").removeClass("invisible");
+					$("#hotTammie").removeClass("invisible");
 					$("#wrapper").removeClass("winter").addClass("summer");
 					$("#vacationIdeas").attr("href", "https://www.airbnb.com/?af=43720035&c=A_TC%3Djmydw8thr5%26G_MT%3Db%26G_CR%3D19944542536%26G_N%3Dg%26G_K%3D%2Bair%20%2Bbnb%26G_P%3D%26G_D%3Dc&gclid=CjwKEAiA9uaxBRDYr4_hrtC3tW8SJAD6UU8GiANrBwuak5j42k1L9KrT7_Q-8PzbUixzhajnnRZHDRoCrm3w_wcB&dclid=CKzjtYS39sgCFTEHRQodbVcByA")
-						 .text("chill out");				
+						 .text("chill out").attr("type", "button");				
 					}
 					$("#temp").text("current temp " + temp + "F");	
 					$("#icon").attr("src", icon), 
